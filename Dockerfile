635a1135ec56f05851f0b0caa655fe72a631ac5e
Vishalkks <vishalkks@gmail.com>
2023-02-19 18:52:00 -0800

    docker change

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,11 @@ WORKDIR /app
 # Upgrade PIP
 RUN pip install --upgrade pip
 
+RUN apt-get update && apt install software-properties-common -y
+RUN add-apt-repository ppa:deadsnakes/ppa -y
+RUN apt install -y build-essential
+RUN apt-get -y install libpq-dev
+
 #Install python libraries from requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
--- app.py
+++ app.py
@@ -55,53 +55,54 @@ def create_app(dev):
 	return app
 
 
+dev = False
+if len(sys.argv) > 1:
+	dev = sys.argv[1] == 'dev'
+app = create_app(dev)
+
+api = Api(app)
+
+# Return JSON of all users
+@app.route('/users')
+def users():
+	users = User.query.all()
+	user_list = []
+	for user in users:
+		user_list.append(user.to_dict())
+	return json.dumps(user_list)
+
+# Return JSON of all houses
+@app.route('/houses')
+def houses():
+	houses = House.query.all()
+	house_list = []
+	for house in houses:
+		house_list.append(house.to_dict())
+	return json.dumps(house_list)
+
+# Return JSON of all house leases
+@app.route('/house_leases')
+def house_leases():
+	house_leases = HouseLease.query.all()
+	lease_list = []
+	for lease in house_leases:
+		lease_list.append(lease.to_dict())
+	return json.dumps(lease_list)
+
+# Return JSON of all house images
+@app.route('/house_images')
+def house_images():
+	house_images = HouseImage.query.all()
+	image_list = []
+	for image in house_images:
+		image_list.append(image.to_dict())
+	return json.dumps(image_list)
+
+class HomeAPI(Resource):
+	def get(self):
+		return render_template('home.html')
+
+api.add_resource(HomeAPI, '/')
+
 if __name__ == '__main__':
-	dev = False
-	if len(sys.argv) > 1:
-		dev = sys.argv[1] == 'dev'
-	app = create_app(dev)
-	
-	api = Api(app)
-
-	# Return JSON of all users
-	@app.route('/users')
-	def users():
-		users = User.query.all()
-		user_list = []
-		for user in users:
-			user_list.append(user.to_dict())
-		return json.dumps(user_list)
-
-	# Return JSON of all houses
-	@app.route('/houses')
-	def houses():
-		houses = House.query.all()
-		house_list = []
-		for house in houses:
-			house_list.append(house.to_dict())
-		return json.dumps(house_list)
-
-	# Return JSON of all house leases
-	@app.route('/house_leases')
-	def house_leases():
-		house_leases = HouseLease.query.all()
-		lease_list = []
-		for lease in house_leases:
-			lease_list.append(lease.to_dict())
-		return json.dumps(lease_list)
-
-	# Return JSON of all house images
-	@app.route('/house_images')
-	def house_images():
-		house_images = HouseImage.query.all()
-		image_list = []
-		for image in house_images:
-			image_list.append(image.to_dict())
-		return json.dumps(image_list)
-
-	class HomeAPI(Resource):
-		def get(self):
-			return render_template('home.html')
-
-	api.add_resource(HomeAPI, '/')
 	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
