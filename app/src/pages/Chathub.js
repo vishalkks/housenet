3d17069e241e3acc7f23f9f7bc6934c068d658f2
Brian Wu <15759684259@163.com>
2023-02-24 15:10:16 -0800

    delete .js files, keep .jsx

--- app/src/pages/Chathub.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Chathub = () => {
-  return <div>This is the Chathub page.</div>;
-};
-
-export default Chathub;
--- app/src/pages/Search.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Search = () => {
-  return <div>This is the search page.</div>;
-};
-
-export default Search;
