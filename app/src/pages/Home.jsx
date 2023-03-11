d2c54733c9370fc425a34f3d2433b0f93d91ae73
kunjchampaneri4 <kchampaneri@ucsd.edu>
2023-03-10 16:38:25 -0800

    Cleared the main page and deleted unused items

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Form, Input, Button } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
 import background from "../static/main_home_background.png";
 
 const Home = () => {
@@ -31,47 +29,6 @@ const Home = () => {
       >
         Explore our range of beautiful properties with the addition of separate
         accommodation suitable for you.
-      </div>
-      <div id="search">
-        <div
-          style={{
-            fontSize: "20px",
-            color: "rgba(105, 185, 157, 0.8)",
-            textAlign: "left",
-            padding: "3% 40% 0% 10%",
-          }}
-        >
-          <b>Rentals Location</b>
-        </div>
-        <div
-          style={{
-            fontSize: "20px",
-            textAlign: "left",
-            padding: "1% 40% 0% 10%",
-          }}
-        >
-          <Form
-            name="customized_form_controls"
-            layout="inline"
-            // onFinish={onSearch}
-          >
-            <Form.Item name="filter" style={{ width: 500 }}>
-              <Input placeholder="Enter an address, city or zip code" />
-            </Form.Item>
-            <Form.Item>
-              <Button
-                htmlType="submit"
-                style={{
-                  color: "white",
-                  backgroundColor: "rgba(105, 185, 157, 0.8)",
-                }}
-                icon={<SearchOutlined />}
-              >
-                Search
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
       </div>
     </div>
   );
